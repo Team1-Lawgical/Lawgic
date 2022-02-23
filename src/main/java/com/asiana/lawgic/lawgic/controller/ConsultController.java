b6d92761503a22b1c31cdc1f3870549d6157f0bd
Jaeuk Oh <wooko5@naver.com>
2022-02-23 17:49:27 +0900

    chore: consult 컨트롤러 수정

--- src/main/java/com/asiana/lawgic/lawgic/controller/ConsultController.java
+++ src/main/java/com/asiana/lawgic/lawgic/controller/ConsultController.java
@@ -1,7 +1,31 @@
 package com.asiana.lawgic.lawgic.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.asiana.lawgic.lawgic.converter.ConsultConverter;
+import com.asiana.lawgic.lawgic.dto.ConsultDTO;
+import com.asiana.lawgic.lawgic.repository.ChatRepository;
+import com.asiana.lawgic.lawgic.service.ConsultServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ConsultController {
+
+    private final ConsultServiceImpl consultService;
+
+    public ConsultController(ConsultServiceImpl consultService) {
+        this.consultService = consultService;
+    }
+
+
+    @PostMapping("/form/consult")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String insertConsultation(ConsultDTO consultDTO, Model model) throws Exception {
+        consultService.insertConsult(consultDTO);
+        int chatId = 1; // 임시값
+        model.addAttribute("chatId", chatId);
+        return "chat/index";
+    }
+
+
 }
