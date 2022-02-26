d0464f52ae20c327fbcb45f5384633a64cac9b2b
YoungKyonYou <man.spride1324@gmail.com>
2022-02-26 23:26:06 +0900

    no message

--- src/test/java/com/asiana/lawgic/lawgic/ConsultRepositoryTest.java
+++ src/test/java/com/asiana/lawgic/lawgic/ConsultRepositoryTest.java
@@ -21,12 +21,13 @@ import java.util.stream.IntStream;
 public class ConsultRepositoryTest {
     private String[] appointmentDate = {"2022-01-16", "2022-06-25", "2022-04-12", "2022-01-23", "2022-01-11"};
     private String[] opponentPhone={"01034523623","01046234324","01045233734","01036239385","01035236343"};
-    private String[] opponentName={"이윤성","서유리","정다은","김은성","고윤정"};
+    private String[] opponentName={"김예리","고은지","정다연","김윤환","유인아"};
     private CarType[] opponentCarType={ CarType.CITY_CAR, CarType.COMPACT_CAR, CarType.SPORTS_CAR, CarType.LARGE_SIZE_CAR, CarType.FULL_SIZE_LUXURY_CAR };
-    private String[] opponentBirthday = {"1995-03-16", "1996-04-25", "1997-12-24", "1993-06-23", "1997-02-12"};
-    private String[] address = {"경기도 시흥시", "경기도 화성시", "서울시 화곡동", " 서울시 염창동", "서울시 관악구"};
-    private String[] judgement = {"경기도 시흥시", "경기도 화성시", "서울시 화곡동", " 서울시 염창동", "서울시 관악구"};
-    private String[] comment = {"경기도 시흥시", "경기도 화성시", "서울시 화곡동", " 서울시 염창동", "서울시 관악구"};
+    private String[] opponentBirthday = {"1991-02-16", "1990-01-22", "1999-10-11", "1993-12-21", "1991-02-19"};
+    private String[] summary={"저는 2차로에서 주행하고, 상대방은 1차로에서 2차로 변경하던 중 후미 사이드와 충돌했습니다. 상대방은 본인 과실 100으로 인정했는데 상대측 보험사에서 갑자기 80:20을 주장합니다. 제가 20% 잘못이 있는지 상담 요청드립니다.\n","저는 로터리에서 주행하고, 상대방은 로터리에 진입하던 중에 전방 충돌했습니다. 상대방은 본인 과실 100으로 인정했는데 상대측 보험사에서 갑자기 50:50을 주장합니다. 제가 50% 잘못이 있는지 상담 요청드립니다.","저는 영동고속도로 가차선에서 주행하고, 상대방은 차선 변경 중에 후미와 충돌했습니다. 상대방은 본인 과실 50으로 인정했는데, 제가 50%나 잘못이 있는지 상담 요청드립니다.\n"};
+    private String[] address = {"경기도 시흥시 능곡동 809 시흥능곡지구 수질복원센터", "경기도 화성시 남양읍 장덕리 801", "경기도 수원시 권선구 세류동 1101-1 수원 채움 요양원", "경기도 성남시 분당구 서현동 277 분당세무서", "경기도 안양시 동안구 호계동 898-21"};
+    private String[] judgement = {"상대방 과실이 100임을 인정받고자 합니다.", "상대방 과실이 80임을 인정받고자 합니다.", "상대방 과실이 50임을 인정받고자 합니다.\n", " 서울시 염창동", "서울시 관악구"};
+    private String[] comment = {"분심위까지 가는 상황은 만들고 싶지 않습니다. 잘 부탁드립니다.", "소송까지 가는 상황은 만들고 싶지 않습니다. 잘 부탁드립니다.", "미디어에 노출 되는 상황은 만들고 싶지 않습니다. 잘 부탁드립니다.", " 서울시 염창동", "서울시 관악구"};
     @Autowired
     private ConsultRepository consultRepository;
     @Autowired
@@ -38,7 +39,7 @@ public class ConsultRepositoryTest {
         Object[] lawyers=lawyerRepository.getAllLawyers();
         Object[] clients=clientRepository.getAllClients();
 
-        IntStream.rangeClosed(1, 5).forEach(i -> {
+        IntStream.rangeClosed(1, 3).forEach(i -> {
             Lawyer lawyer=((Lawyer)lawyers[i-1]);
             Client client=((Client)clients[i-1]);
             Consult consult=Consult.builder()
@@ -51,8 +52,9 @@ public class ConsultRepositoryTest {
                     .opponentCarType(opponentCarType[i-1])
                     .opponentBirthday(Date.valueOf(opponentBirthday[i-1]))
                     .opponentAddress(address[i-1])
-                    .judgement("judgement "+i)
-                    .comments("comment "+i)
+                    .judgement(judgement[i-1])
+                    .comments(comment[i-1])
+                    .summary(summary[i-1])
                     .build();
             consultRepository.save(consult);
 
